4b97aa1bcd8293450c344e1b90a81f225ea1580d
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2008-10-01 15:23:18 +0000

    GUI project for tests added

---
+++ ParserGUI/MainWindow.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ParserGUI
+{
+	partial class MainWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panelControls = new System.Windows.Forms.Panel();
+			this.panelLayuot = new System.Windows.Forms.Panel();
+			this.buttonParse = new System.Windows.Forms.Button();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.textBoxSource = new System.Windows.Forms.TextBox();
+			this.webBrowserOutPut = new System.Windows.Forms.WebBrowser();
+			this.panelControls.SuspendLayout();
+			this.panelLayuot.SuspendLayout();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panelControls
+			// 
+			this.panelControls.Controls.Add(this.buttonParse);
+			this.panelControls.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panelControls.Location = new System.Drawing.Point(0, 392);
+			this.panelControls.Name = "panelControls";
+			this.panelControls.Size = new System.Drawing.Size(526, 46);
+			this.panelControls.TabIndex = 0;
+			// 
+			// panelLayuot
+			// 
+			this.panelLayuot.Controls.Add(this.splitContainer1);
+			this.panelLayuot.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelLayuot.Location = new System.Drawing.Point(0, 0);
+			this.panelLayuot.Name = "panelLayuot";
+			this.panelLayuot.Size = new System.Drawing.Size(526, 392);
+			this.panelLayuot.TabIndex = 1;
+			// 
+			// buttonParse
+			// 
+			this.buttonParse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonParse.Location = new System.Drawing.Point(439, 11);
+			this.buttonParse.Name = "buttonParse";
+			this.buttonParse.Size = new System.Drawing.Size(75, 23);
+			this.buttonParse.TabIndex = 0;
+			this.buttonParse.Text = "Parse";
+			this.buttonParse.UseVisualStyleBackColor = true;
+			this.buttonParse.Click += new System.EventHandler(this.buttonParse_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.textBoxSource);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.webBrowserOutPut);
+			this.splitContainer1.Size = new System.Drawing.Size(526, 392);
+			this.splitContainer1.SplitterDistance = 259;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// textBoxSource
+			// 
+			this.textBoxSource.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.textBoxSource.Location = new System.Drawing.Point(0, 0);
+			this.textBoxSource.Multiline = true;
+			this.textBoxSource.Name = "textBoxSource";
+			this.textBoxSource.Size = new System.Drawing.Size(259, 392);
+			this.textBoxSource.TabIndex = 0;
+			// 
+			// webBrowserOutPut
+			// 
+			this.webBrowserOutPut.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowserOutPut.Location = new System.Drawing.Point(0, 0);
+			this.webBrowserOutPut.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowserOutPut.Name = "webBrowserOutPut";
+			this.webBrowserOutPut.Size = new System.Drawing.Size(263, 392);
+			this.webBrowserOutPut.TabIndex = 0;
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(526, 438);
+			this.Controls.Add(this.panelLayuot);
+			this.Controls.Add(this.panelControls);
+			this.Name = "MainWindow";
+			this.Text = "GUI for Parser.NET test";
+			this.Load += new System.EventHandler(this.MainWindow_Load);
+			this.panelControls.ResumeLayout(false);
+			this.panelLayuot.ResumeLayout(false);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panelControls;
+		private System.Windows.Forms.Button buttonParse;
+		private System.Windows.Forms.Panel panelLayuot;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TextBox textBoxSource;
+		private System.Windows.Forms.WebBrowser webBrowserOutPut;
+	}
+}
+
---
+++ ParserGUI/MainWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using Parser;
+using Parser.Model;
+using Parser.Util;
+
+namespace ParserGUI
+{
+	public partial class MainWindow : Form
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWindow_Load(object sender, EventArgs e)
+		{
+			textBoxSource.Text = @"
+@main[]
+	$myvar[xxx]
+	test is ^test[what;777]
+
+@test[word;number]
+	tested $word $number $myvar
+";
+		}
+
+		private void buttonParse_Click(object sender, EventArgs e)
+		{
+			SourceBuilder builder = new SourceBuilder();
+			builder.Parse(textBoxSource.Text);
+			Dumper d = new Dumper();
+
+			string dump = d.Dump((RootNode)builder.RootNode).ToString();
+
+			Executor exec = new Executor();
+			exec.Run((RootNode)builder.RootNode);
+			string actual = exec.Output.ToString();
+
+			// System.Windows.Forms.WebBrowser
+			webBrowserOutPut.DocumentText =
+				String.Format("{0}<hr/><pre>{1}</pre>", actual, dump);
+			
+		}
+	}
+}
