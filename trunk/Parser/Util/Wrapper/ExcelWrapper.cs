811d48b95bf6a47f0ed7ddc8d37e671deda46327
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2008-10-11 17:03:15 +0000

    some classes restored

---
+++ trunk/Parser/Util/Wrapper/ExcelWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Parser.NET.Model;
+
+namespace Parser.NET.Util.Wrapper
+{
+	/// <summary>
+	/// Предоставляет фасад для доступа к excel через ADO.NET
+	/// </summary>
+	public class ExcelWrapper
+	{
+
+	}
+}
---
+++ trunk/ParserTest/Model/TableTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Parser.Model;
+
+namespace Parser.NETTest.Model
+{
+	[TestFixture]
+	public class TableTest
+	{
+		[Test]
+		public void SetAndGetCellsTest()
+		{
+			Table<string> t = new Table<string>();
+			t.Add(0, 0, "first");
+			t.Add(0, 1, "second");
+			t.Add(1, 0, "third");
+			t.Add(1, 1, "fourth");
+			//foreach (string s in t)
+			//{
+				
+			//}
+			Console.WriteLine(t[0, 0]);
+			Console.WriteLine(t[0, 1]);
+			Console.WriteLine(t[1, 0]);
+			Console.WriteLine(t[1, 1]);
+			// TODO assert foreach
+		}
+	}
+}
