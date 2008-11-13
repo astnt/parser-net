1a4a810a8fe1913b552274b7ef653a45c64fc2ce
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2008-11-13 06:50:05 +0000

    vars access test added

---
+++ ParserTest/SourceBuilder/VarsAccessTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Parser.Model;
+using Parser.Util;
+using ParserTest;
+
+namespace Parser.NETTest.SourceBuilder
+{
+	[TestFixture]
+	public class VarsAccessTest : AbstractParserTest
+	{
+		[Test]
+		public void SimpleVarTest()
+		{
+			string source = @"
+@main[]
+	$sb.ToString()
+";
+			Parser.SourceBuilder builder = new Parser.SourceBuilder();
+			builder.Parse(source);
+			Dumper d = new Dumper();
+			Model(d.Dump((RootNode)builder.RootNode));
+
+			Executor exec = new Executor();
+			exec.Run((RootNode)builder.RootNode);
+			string actual = exec.Output.ToString();
+
+			Result(actual);
+		}
+
+
+
+	}
+}
