92addbad42477c8f000d010d7ae2b370fcc88fdc
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2008-11-23 06:24:11 +0000

    executor interface added

---
+++ Parser/Model/IExecutable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser.Model
+{
+	/// <summary>
+	/// Отмечает модели, содержащие <see cref="Executor"/>,
+	/// и выполняющие код дерева парсера.
+	/// </summary>
+	interface IExecutable
+	{
+		void AddExecutor(Executor executor);
+	}
+}
