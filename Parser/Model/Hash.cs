a4f6a55057ee1303e8f4fafdb5506aff2f96361d
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2008-12-20 10:39:32 +0000

    hash class added

---
+++ Parser/Model/Hash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser.Model
+{
+	public class Hash
+	{
+//		private Dictionary<StringBuilder, Object> dictionary = new  Dictionary<StringBuilder, Object>();
+		private Dictionary<String, Object> dictionary = new  Dictionary<String, Object>();
+		public Hash()
+		{
+			
+		}
+		public void addKey(StringBuilder key, StringBuilder value)
+		{
+			if(dictionary.ContainsKey(key.ToString()))
+			{
+				dictionary[key.ToString()] = value;
+			}
+			else
+			{
+				dictionary.Add(key.ToString(), value);
+			}
+		}
+		public string getKey(StringBuilder key)
+		{
+			return dictionary[key.ToString()].ToString();
+		}
+	}
+}
