e009e05b5ecd757c4c9e4d6452c8b1f8dcc22ab3
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2008-12-25 10:25:43 +0000

    naming fixed, test added

---
+++ ParserTest/SourceBuilderTests/NamingTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ParserTest.SourceBuilderTests
+{
+	[TestFixture]
+	public class NamingTest : AbstractParserTest
+	{
+		[Test]
+		public void NameInXmlAttributeTest()
+		{
+			string actual = Parse(@"@main[] $var[value] <option value=""$var"">$var</option>");
+			Result(actual);
+			Assert.AreEqual(actual, @"  <option value=""value"">value</option>");
+		}
+	}
+}
