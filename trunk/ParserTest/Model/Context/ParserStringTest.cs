1b460d8c226013d2feacfcb19551a7c74cf541ca
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2008-12-30 11:09:31 +0000

    parser string test added

---
+++ trunk/ParserTest/Model/Context/ParserStringTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace ParserTest.Model.Context
+{
+	[TestFixture]
+	public class ParserStringTest : AbstractParserTest
+	{
+//		Алтайский край (не включая г. Барнаул, г. Бийск, г. Новоалтайск и г. Рубцовск)
+//		г. Барнаул
+		[Test]
+		public void PositionAndContainsTest()
+		{
+			string actual = Parse(@"
+@main[]
+	$var[Алтайский край (не включая г. Барнаул, г. Бийск, г. Новоалтайск и г. Рубцовск)]
+	^if(^var.contains[не включая]){^var.mid[0;^var.pos[не включая]]}
+");
+			Result(actual);
+			Assert.IsTrue(actual.Contains("Алтайский край"));
+			Assert.IsTrue(!actual.Contains("не включая"));
+			Assert.IsTrue(!actual.Contains("г. Барнаул"));
+		}
+	}
+}
