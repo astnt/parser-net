344e03b62c527613fbe367fe2cac0376d2eeaf23
anton.starcev <anton.starcev@bf401268-8d5a-11dd-a205-0939b8ba2825>
2009-02-04 03:50:22 +0000

    Enum added

---
+++ Parser/Model/Context/Enum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parser.Model.Context
+{
+	/// <summary>
+	/// Класс для перебора IEnumerable
+	/// </summary>
+	public class Enum : IExecutable
+	{
+		private List<Object> list = new List<object>();
+		private Executor exec;
+		public void AddExecutor(Executor executor)
+		{
+			exec = executor;
+		}
+	}
+}
